18bb23c22e532200c9026a44ac83d3abd0275555
wm129 <sekaimanyuucamp@gmail.com>
2020-12-07 17:19:00 +0900

    store/action

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{FC, useEffect} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+
+const App: FC = (props) => {
+
+   // const DOG_API = 'https://dog.ceo/api/breed/Pembroke/images/random';//
+/*useEffect(() => {
+    props.
+}, []);
+*/
+
+
+    return(
+        <>
+<div className="App">
+    <h1>DOGAPI 練習</h1>
+    
+</div>
+        </>
+    );
+};
 export default App;
